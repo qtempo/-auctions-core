0868e7136d29bdf1b8d996dd5b094487447f5cd6
yaroslavprt <yaroslavprt@gmail.com>
2024-06-27 16:53:12 +0300

    fix imports

--- src/auctions/user-cases/get-auctions-by-status/get-auctions-by-status.port.ts
+++ src/auctions/user-cases/get-auctions-by-status/get-auctions-by-status.port.ts
@@ -1,4 +1,4 @@
-import { Query } from '../../core/base.query'
-import { Auction } from '../domain/auction'
+import { Query } from '../../../core/base.query'
+import { Auction } from '../../domain/auction'
 
 export interface GetAuctionsByStatusPort extends Query<Auction['status'], Auction[]> {}
